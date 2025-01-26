37bf0eb69a3676050d0d8161f55ff1320e806939
jkusuda <168035390+jkusuda@users.noreply.github.com>
2025-01-25 23:58:51 -0500

    variables

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from Chat_API import *  # Assuming the necessary functions are in this module
-
+import ast
 app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
-db = SQLAlchemy(app)
-
-class Variables(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    variables = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"Variables('{self.name}', '{self.value}')"
+VARIABLES = None
 
 # Route to serve the HTML file
 @app.route('/')
@@ -19,18 +12,21 @@ def home():
 # Route to handle the 'Solve' button functionality
 @app.route('/solve', methods=['POST'])
 def solve_route():
-    if request.method == "POST":
-        # Get the problem data from the frontend
-        data = request.get_json()
-        problem = data.get('problem', '')
-        print('solve button clicked')
+    # Get the problem data from the frontend
+    data = request.get_json()
+    problem = data.get('problem', '')
+    print('solve button clicked')
 
     # Solve the problem and get the solution and steps
     solution, steps, variables = process_physics_response(problem)
-    print(solution)
+    #print(solution)
     global VARIABLES
     VARIABLES = variables
-    print(VARIABLES)
+    
+    val = VARIABLES.find("{")
+    val2 = VARIABLES.find("}")
+    VARIABLES = ast.literal_eval(VARIABLES[val:val2+1])
+
     # Return the solution and steps as JSON
     return jsonify({
         'solution': solution,
